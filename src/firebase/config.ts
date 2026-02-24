1db3eef5adcac0d7d0ec9f94c60d388c7d6b625b
Grindwear <grindwearafrica@gmail.com>
2026-02-24 02:35:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9295758728-d3ddf",
+  "appId": "1:380796081096:web:d85fac8f3c65dbd839e6fd",
+  "apiKey": "AIzaSyCYULwY9iQJ-R0fOBiJbwtOo67DTcqwPOc",
+  "authDomain": "studio-9295758728-d3ddf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "380796081096"
+};
