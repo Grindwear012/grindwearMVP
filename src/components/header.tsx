50afb962768061dccc67c6b7f6068cbac33e2c09
Grindwear <grindwearafrica@gmail.com>
2026-02-24 03:06:27 +0000

    ohk in the above where you wrote home , tops , bottoms ,outwear , dresse

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,10 +36,9 @@ export default function Header() {
 
   const navItems = [
     { name: 'Home', href: '/' },
-    { name: 'Tops', href: '/products?category=Tops' },
-    { name: 'Bottoms', href: '/products?category=Bottoms' },
-    { name: 'Outerwear', href: '/products?category=Outerwear' },
-    { name: 'Dresses', href: '/products?category=Dresses' },
+    { name: 'Men', href: '/products?category=Men' },
+    { name: 'Women', href: '/products?category=Women' },
+    { name: 'Accessories', href: '/products?category=Accessories' },
   ];
 
   const handleSignOut = async () => {
@@ -53,7 +52,7 @@ export default function Header() {
       <div className="container flex h-16 items-center">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center">
-            <Logo className="h-12" />
+            <Logo className="h-16" />
           </Link>
           <nav className="flex items-center space-x-6 text-sm font-medium">
             {navItems.map((item) => (
@@ -78,7 +77,7 @@ export default function Header() {
             </SheetTrigger>
             <SheetContent side="left">
                <Link href="/" className="mr-6 flex items-center mb-6">
-                <Logo className="h-12" />
+                <Logo className="h-16" />
               </Link>
               <nav className="flex flex-col space-y-4">
                  {navItems.map((item) => (
